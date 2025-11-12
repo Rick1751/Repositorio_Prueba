58b55253daed4ac3822d59c19c67cdd83a006e94
Rick1751 <leonr7393@gmail.com>
2025-11-12 14:57:53 -0500

    Mi primer comit en GitHub

---
+++ Proyecto/src/Paquete_1/Ejercicio.java
@@ -0,0 +1,18 @@
+package Paquete_1;
+
+public class Ejercicio {
+
+	public static void main(String[] args) {
+
+//		Bucles  en modo tabla 
+		
+		for (int i = 1; i < 4; i++) {
+
+			for (int j = 1; j < 4; j++) {
+				System.out.print(i + j + " ");
+			}
+			System.out.println();
+		}
+		
+	}
+}
