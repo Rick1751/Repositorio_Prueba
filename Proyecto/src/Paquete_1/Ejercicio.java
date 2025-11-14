6209ccd1a3303ed14ffdb12c6ff2c3c0b9693c4f
Rick1751 <leonr7393@gmail.com>
2025-11-13 19:04:34 -0500

    igual

--- Proyecto/src/Paquete_1/Ejercicio.java
+++ Proyecto/src/Paquete_1/Ejercicio.java
@@ -2,8 +2,11 @@ package Paquete_1;
 
 public class Ejercicio {
 
-	public static void main(String[] args) {
 
+//	Funcion de llamada de bucles 
+	
+	public static void llamadoBucles() {
+		
 //		Bucles  en modo tabla 
 		
 		for (int i = 1; i < 4; i++) {
@@ -12,7 +15,9 @@ public class Ejercicio {
 				System.out.print(i + j + " ");
 			}
 			System.out.println();
-		}
+		
 		
 	}
-}
+	}
+	}
+
--- Proyecto/src/Paquete_1/Ejercicio_2.java
+++ Proyecto/src/Paquete_1/Ejercicio_2.java
@@ -4,7 +4,14 @@ public class Ejercicio_2 {
 
 	
 	public static void main(String[] args) {
+		
+		Ejercicio ejercicio2 = new Ejercicio();
+		
 		System.out.println("Hola Mundo");
 		System.out.println("Estoy aprendiendo Java");
+		System.out.println("Estoy en el curso de Programacion");
+		
+		ejercicio2.llamadoBucles();
 	}
+
 }
